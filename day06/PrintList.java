aa53ec19082f273ecaa0a8c5cca764e31e601039
Marco <marco.mangan@gmail.com>
2024-08-21 22:06:57 +0000

    This change adds a simple recursive solution to print a list.

--- day06/PrintList.java
+++ day06/PrintList.java
@@ -2,15 +2,27 @@ import java.util.Arrays;
 
 class PrintList 
 {
+    private static void print0(int[] vs, int i) {
+        if (i < vs.length) {
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(vs[i]);
+            print0(vs, i + 1);
+        }
+    }
 
-    private void print(int[] vs) {
+    private static void print(int[] vs) {
         // mostra o conteudo da lista
+        System.out.print("[");
         print0(vs, 0);
+        System.out.print("]");
     }
 
     public static void main(String[] args) {
         System.out.println("PrintList");
         int[] vs = {23, 43, 10, 89};
         System.out.println(Arrays.toString(vs));
+        print(vs);
     }
 }
