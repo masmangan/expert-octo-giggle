e17f1065b8bf3e2ef17cb7da5e32925c4f0e8c5e
Marco <marco.mangan@gmail.com>
2024-09-11 21:40:09 +0000

    This change adds a recursive exampĺe.

--- day06/PrintList.java
+++ day06/PrintList.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 
-class PrintList 
-{
+class PrintList {
     private static void print0(int[] vs, int i) {
         if (i < vs.length) {
             if (i != 0) {
@@ -12,17 +11,21 @@ class PrintList
         }
     }
 
-    private static void print(int[] vs) {
-        // mostra o conteudo da lista
-        System.out.print("[");
-        print0(vs, 0);
-        System.out.print("]");
+    public static void print(int[] vs) {
+        if (vs == null) {
+            System.out.print("null");
+        } else {
+            System.out.print("[");
+            print0(vs, 0);
+            System.out.print("]");
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("PrintList");
-        int[] vs = {23, 43, 10, 89};
+        int[] vs = {20,10,40,50};
         System.out.println(Arrays.toString(vs));
         print(vs);
+        System.out.println();
     }
 }
