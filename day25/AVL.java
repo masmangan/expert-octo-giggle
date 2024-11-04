9a2a9c07601a5de46c03d68fe932768895e71f9a
Marco <marco.mangan@gmail.com>
2024-11-04 22:02:19 +0000

    This change adds a simple and a double rotation with a right child.

--- day25/AVL.java
+++ day25/AVL.java
@@ -43,14 +43,31 @@ public class AVL {
             n.right = add0(v, n.right);
             // Está equilibrada?
             if (h(n.right) - h(n.left) >= 2) {
-                System.out.println("Desequilibrada!");
+                System.out.printf("Desequilibrada! R%d%n", n.value);
+                if (n.right.value > v) {
+                    System.out.println("DUPLA!");
+                    Node a = n.right;
+                    Node b = a.left;
+                    a.left = b.right;
+                    b.right = a;
+                    n.right = b;
+                    a.height = Math.max(h(a.left), h(a.right)) + 1;
+                    b.height = Math.max(h(b.left), h(b.right)) + 1;
+                    }
+                Node x = n;
+                Node y = x.right;
+                x.right = y.left;
+                y.left = x;
+                x.height = Math.max(h(x.left), h(x.right)) + 1;
+                y.height = Math.max(h(y.left), h(y.right)) + 1;
+                return y;
             }
             n.height = Math.max(h(n.left), h(n.right)) + 1;
         } else if (n.value > v) {
             n.left = add0(v, n.left);
             // Está equilibrada?            
             if (h(n.left) - h(n.right) >= 2) {
-                System.out.println("Desequilibrada!");
+                System.out.printf("Desequilibrada! L%d%n", n.value);
             }            
             n.height = Math.max(h(n.left), h(n.right)) + 1;
         }
@@ -94,11 +111,11 @@ public class AVL {
 
         AVL t1 = new AVL();
         System.out.println(t1);
-        t1.add(20);
+        t1.add(10);
         System.out.println(t1);
         t1.add(30);
         System.out.println(t1);
-        t1.add(10);
+        t1.add(20);
         System.out.println(t1);
     }
 
